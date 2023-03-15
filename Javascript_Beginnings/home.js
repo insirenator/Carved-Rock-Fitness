001fd0873e8a4460c68b7404cbfd7b51dff134c9
insirenator <arsshakeeb149@protonmail.com>
2023-03-15 10:55:08 +0530

    Added : Javascript_Beginnings

---
+++ Javascript_Beginnings/home.js
@@ -0,0 +1,23 @@
+// Creates an alert when the homepage loads
+alert("Welcome");
+
+// Store a string in a variable and then display it
+let greeting = "Welcome (using a variable)";
+
+alert(greeting);
+
+// Always format the code for better readability.
+
+/*
+	Javascript is case-sensitive, which means : 
+	greet, Greet, and GREET are all different variables.
+	Look at the code below.
+*/
+
+let greet = "hello";
+let Greet = "Hello";
+let GREET = "HELLO";
+
+console.log("greet = ", greet);
+console.log("Greet = ", Greet);
+console.log("GREET = ", GREET);
